22cdb69a9c833c9aae29c6266b37ed62afaf893d
venysssssssssss <shablaur7@hotmail.com>
2024-05-21 13:28:40 -0300

    refactor: Separate ClickUp data retrieval and organization logic

--- fast.py
+++ fast.py
@@ -11,42 +11,40 @@ app = FastAPI()
 
 @app.get('/get_clickup_data/{list_id}')
 async def get_clickup_data(list_id: str):
-    # Define o caminho da pasta onde os dados serão salvos
+    """
+    Retrieves data from ClickUp API for a given list ID and saves it to a CSV file.
+
+    Args:
+        list_id (str): The ID of the ClickUp list.
+
+    Returns:
+        dict: A dictionary containing the success message and the data structure.
+    """
     data_folder = 'data'
 
-    # Verifica se a pasta 'data' existe e a cria se não existir
     if not os.path.exists(data_folder):
         os.makedirs(data_folder)
 
     url = f'https://api.clickup.com/api/v2/list/{list_id}/task'
 
-    # Parâmetros da consulta
     query = {
         'archived': 'false',
         'include_markdown_description': 'true',
     }
 
-    # Cabeçalhos da solicitação
     headers = {'Authorization': 'pk_43030192_303UC2Z0VJEJ5QY9ES23X8I22ISAHUX2'}
 
-    # Fazendo a solicitação
     response = requests.get(url, headers=headers, params=query)
 
-    # Verificando se a solicitação foi bem-sucedida
     if response.status_code == 200:
-        # Convertendo a resposta JSON em um DataFrame pandas
         data = response.json()
         tasks = data['tasks']
 
-        # Lista para armazenar os dados das tarefas
         task_data = []
 
-        # Percorre cada tarefa
         for task in tasks:
-            # Dicionário para armazenar os dados da tarefa atual
             task_dict = {}
 
-            # Adiciona os campos principais ao dicionário da tarefa
             task_dict['id'] = task['id']
             task_dict['name'] = task['name']
             task_dict['text_content'] = task['text_content'].replace('\n', ' ')
@@ -55,32 +53,24 @@ async def get_clickup_data(list_id: str):
             task_dict['date_updated'] = task['date_updated']
             task_dict['url'] = task['url']
 
-            # Percorre cada campo personalizado na tarefa
             for field in task['custom_fields']:
-                # Adiciona o valor do campo personalizado ao dicionário da tarefa
                 if 'value' in field:
                     task_dict[field['name']] = field['value']
                 else:
                     task_dict[field['name']] = None
 
-            # Adiciona o dicionário da tarefa à lista de dados das tarefas
             task_data.append(task_dict)
 
-        # Converte a lista de dados das tarefas em um DataFrame pandas
         df = pd.DataFrame(task_data)
 
-        # Substitui valores de ponto flutuante não compatíveis com JSON
         df.replace([np.inf, -np.inf, np.nan], 0, inplace=True)
 
-        # Define o caminho completo do arquivo CSV
         csv_file_path = os.path.join(
             data_folder, f'dados_clickup_{list_id}.xlsx'
         )
 
-        # Salva o DataFrame em um arquivo CSV
         df.to_excel(csv_file_path, index=False)
 
-        # Retorna a estrutura dos dados e a mensagem de sucesso
         return {
             'message': f'Dados salvos com sucesso em "{csv_file_path}"',
             'data': df.to_dict(),
@@ -94,32 +84,33 @@ async def get_clickup_data(list_id: str):
 
 @app.get('/get_data_organized/{list_id}')
 async def get_clickup_data(list_id: str):
+    """
+    Retrieves data from ClickUp API for a given list ID and organizes it based on specific fields.
+
+    Args:
+        list_id (str): The ID of the ClickUp list.
+
+    Returns:
+        list: A list of dictionaries containing the filtered data.
+    """
     url = f'https://api.clickup.com/api/v2/list/{list_id}/task'
 
-    # Parâmetros da consulta
     query = {
         'archived': 'false',
         'include_markdown_description': 'true',
     }
 
-    # Cabeçalhos da solicitação
     headers = {'Authorization': 'pk_43030192_303UC2Z0VJEJ5QY9ES23X8I22ISAHUX2'}
 
-    # Fazendo a solicitação
     response = requests.get(url, headers=headers, params=query)
 
-    # Verificando se a solicitação foi bem-sucedida
     if response.status_code == 200:
-        # Convertendo a resposta JSON em um dicionário
         data = response.json()
 
-        # Lista para armazenar os dados filtrados
         filtered_data = []
 
-        # Variável para contar projetos
         project_count = 0
 
-        # Lista de nomes de campos conhecidos
         field_names = [
             'CARTEIRA DEMANDANTE',
             'E-MAIL',
@@ -128,6 +119,7 @@ async def get_clickup_data(list_id: str):
             'OBJETIVO DO GANHO',
             'KPI GANHO',
             '💡 TIPO DE PROJETO',
+            'TIPO DE PROJETO',
             'TIPO DE OPERAÇÃO',
             'PRODUTO',
             'OPERAÇÃO',
@@ -142,25 +134,19 @@ async def get_clickup_data(list_id: str):
             'CENÁRIO PROPOSTO',
         ]
 
-        # Percorre cada tarefa
         for task in data['tasks']:
-            # Incrementa o contador de projetos
             project_count += 1
 
-            # Dicionário para armazenar os dados filtrados da tarefa
             filtered_task = {
                 'Projeto': project_count,
                 'ID': task['id'],
                 'Status': task['status']['status'],
             }
 
-            # Texto da tarefa sem quebras de linha
             task_text = task['text_content'].replace('\n', ' ').replace('.:', '')
 
-            # Inicializa um dicionário para armazenar os campos e valores encontrados
             field_values = {field: "" for field in field_names}
 
-            # Divide o texto da tarefa por campos conhecidos
             for field_name in field_names:
                 pattern = re.compile(rf'{re.escape(field_name)}\s*:\s*(.*?)(?=\s*({"|".join([re.escape(name) for name in field_names])})\s*:|$)', re.IGNORECASE)
                 match = pattern.search(task_text)
@@ -168,22 +154,16 @@ async def get_clickup_data(list_id: str):
                     field_value = match.group(1).strip()
                     field_values[field_name] = field_value 
 
-            # Adiciona os campos encontrados ao dicionário filtrado
             filtered_task.update(field_values)
 
-            # Verificação específica para "💡 R$ ANUAL (PREVISTO)"
             if '💡 TIPO DE PROJETO' in filtered_task and '💡 R$ ANUAL (PREVISTO)' in filtered_task['💡 TIPO DE PROJETO']:
                 tipo_projeto_value = filtered_task['💡 TIPO DE PROJETO']
                 tipo_projeto_parts = tipo_projeto_value.split('💡 R$ ANUAL (PREVISTO)')
                 filtered_task['💡 TIPO DE PROJETO'] = tipo_projeto_parts[0].strip()
                 filtered_task['💡 R$ ANUAL (PREVISTO)'] = tipo_projeto_parts[1].strip()
 
-            # Adiciona os dados filtrados à lista
             filtered_data.append(filtered_task)
 
-            print(filtered_task)
-
-        # Retorna os dados filtrados
         return filtered_data
     else:
         raise HTTPException(
