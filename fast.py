d683d34053c67469792b925f56f8d8b5bc8c2445
venysssssssssss <shablaur7@hotmail.com>
2024-05-23 18:11:34 -0300

    optimization

--- fast.py
+++ fast.py
@@ -1,18 +1,13 @@
-import os
 import re
 import httpx
-import numpy as np
-import pandas as pd
-import requests
 from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-
 @app.get('/get_data_organized/{list_id}')
 async def get_clickup_data(list_id: str):
     """
-    Retrieves data from ClickUp API for a given list ID and organizes it based on specific fields.
+    Retrieve and organize data from ClickUp API.
 
     Args:
         list_id (str): The ID of the ClickUp list.
@@ -21,84 +16,56 @@ async def get_clickup_data(list_id: str):
         list: A list of dictionaries containing the filtered data.
     """
     url = f'https://api.clickup.com/api/v2/list/{list_id}/task'
-
-    query = {
-        'archived': 'false',
-        'include_markdown_description': 'true',
-    }
-
+    query = {'archived': 'false', 'include_markdown_description': 'true'}
     headers = {'Authorization': 'pk_43030192_303UC2Z0VJEJ5QY9ES23X8I22ISAHUX2'}
 
-    async with httpx.AsyncClient(timeout=140.0) as client:  # Aumenta o tempo limite para 120 segundos
+    async with httpx.AsyncClient(timeout=140.0) as client:
         response = await client.get(url, headers=headers, params=query)
 
-    if response.status_code == 200:
-        data = response.json()
-        filtered_data = []
-        project_count = 0
+    if response.status_code != 200:
+        raise HTTPException(status_code=400, detail=f'Erro ao fazer a solicitação. Código de status: {response.status_code}')
 
-        field_names = [
-            'CARTEIRA DEMANDANTE',
-            'E-MAIL',
-            'ESCOPO',
-            'OBS',
-            'OBJETIVO DO GANHO',
-            'KPI GANHO',
-            '💡 TIPO DE PROJETO',
-            'TIPO DE PROJETO',
-            'TIPO DE OPERAÇÃO',
-            'PRODUTO',
-            'OPERAÇÃO',
-            'SITE',
-            'UNIDADE DE NEGÓCIO',
-            'DIRETOR TAHTO',
-            'CLIENTE',
-            'TIPO',
-            '💡 R$ ANUAL (PREVISTO)',
-            'GERENTE OI',
-            'FERRAMENTA ENVOLVIDA',
-            'CENÁRIO PROPOSTO',
-        ]
+    data = response.json()
+    filtered_data = []
+    field_names = [
+        'CARTEIRA DEMANDANTE', 'E-MAIL', 'ESCOPO', 'OBS', 'OBJETIVO DO GANHO',
+        'KPI GANHO', '💡 TIPO DE PROJETO', 'TIPO DE PROJETO', 'TIPO DE OPERAÇÃO',
+        'PRODUTO', 'OPERAÇÃO', 'SITE', 'UNIDADE DE NEGÓCIO', 'DIRETOR TAHTO',
+        'CLIENTE', 'TIPO', '💡 R$ ANUAL (PREVISTO)', 'GERENTE OI', 'FERRAMENTA ENVOLVIDA',
+        'CENÁRIO PROPOSTO',
+    ]
 
-        for task in data['tasks']:
-            project_count += 1
+    for project_count, task in enumerate(data['tasks'], start=1):
+        filtered_task = {
+            'Projeto': project_count,
+            'ID': task['id'],
+            'Status': task['status']['status'],
+            'Name': task['name'],
+            'Priority': task.get('priority', {}).get('priority') if task.get('priority') is not None else None,
+            'Líder': task.get('assignees', [{}])[0].get('username') if task.get('assignees') else None,
+            'Email líder': task.get('assignees', [{}])[0].get('email') if task.get('assignees') else None,
+        }
 
-            filtered_task = {
-                'Projeto': project_count,
-                'ID': task['id'],
-                'Status': task['status']['status'],
-                'Name': task['name'],
-            }
+        task_text = task['text_content'].replace('\n', ' ').replace('.:', '')
+        field_values = {field: '' for field in field_names}
 
-            # Extraindo usernames dos assignees
-            if 'assignees' in task:
-                usernames = [assignee['username'] for assignee in task['assignees']]
-                filtered_task['Líder'] = ', '.join(usernames)
+        for field_name in field_names:
+            pattern = re.compile(
+                rf'{re.escape(field_name)}\s*:\s*(.*?)(?=\s*({"|".join([re.escape(name) for name in field_names])})\s*:|$)',
+                re.IGNORECASE,
+            )
+            match = pattern.search(task_text)
+            if match:
+                field_values[field_name] = match.group(1).strip()
 
-            task_text = task['text_content'].replace('\n', ' ').replace('.:', '')
+        filtered_task.update(field_values)
 
-            field_values = {field: "" for field in field_names}
+        if '💡 TIPO DE PROJETO' in filtered_task and '💡 R$ ANUAL (PREVISTO)' in filtered_task['💡 TIPO DE PROJETO']:
+            tipo_projeto_value = filtered_task['💡 TIPO DE PROJETO']
+            tipo_projeto_parts = tipo_projeto_value.split('💡 R$ ANUAL (PREVISTO)')
+            filtered_task['💡 TIPO DE PROJETO'] = tipo_projeto_parts[0].strip()
+            filtered_task['💡 R$ ANUAL (PREVISTO)'] = tipo_projeto_parts[1].strip()
 
-            for field_name in field_names:
-                pattern = re.compile(rf'{re.escape(field_name)}\s*:\s*(.*?)(?=\s*({"|".join([re.escape(name) for name in field_names])})\s*:|$)', re.IGNORECASE)
-                match = pattern.search(task_text)
-                if match:
-                    field_value = match.group(1).strip() 
-                    field_values[field_name] = field_value 
+        filtered_data.append(filtered_task)
 
-            filtered_task.update(field_values)
-
-            if '💡 TIPO DE PROJETO' in filtered_task and '💡 R$ ANUAL (PREVISTO)' in filtered_task['💡 TIPO DE PROJETO']:
-                tipo_projeto_value = filtered_task['💡 TIPO DE PROJETO']
-                tipo_projeto_parts = tipo_projeto_value.split('💡 R$ ANUAL (PREVISTO)')
-                filtered_task['💡 TIPO DE PROJETO'] = tipo_projeto_parts[0].strip()
-                filtered_task['💡 R$ ANUAL (PREVISTO)'] = tipo_projeto_parts[1].strip()
-
-            filtered_data.append(filtered_task)
-
-        return filtered_data
-    else:
-        raise HTTPException(
-            status_code=400,
-            detail=f'Erro ao fazer a solicitação. Código de status: {response.status_code}',
-        )
+    return filtered_data
