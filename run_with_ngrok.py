2627a8b33f0ae78598f7d7dc7c842ff410ebdcf9
venysssssssssss <shablaur7@hotmail.com>
2024-05-23 18:11:41 -0300

    refactor: Update dependencies in pyproject.toml

--- run_with_ngrok.py
+++ run_with_ngrok.py
@@ -1,10 +1,10 @@
-from pyngrok import ngrok
 import uvicorn
+from pyngrok import ngrok
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Iniciar um túnel ngrok na porta 8000
     public_url = ngrok.connect(8000)
-    print(f"ngrok public URL: {public_url}")
+    print(f'ngrok public URL: {public_url}')
 
     # Iniciar o servidor FastAPI usando Uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, log_level='info')
