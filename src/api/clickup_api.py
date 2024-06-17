75ced27b8bcff5719a957f5d605ab328ee3e51a1
venysssssssssss <shablaur7@hotmail.com>
2024-06-17 11:12:48 -0300

    chore: Apply cache logic with Redis for improved performance in render server

--- src/api/clickup_api.py
+++ src/api/clickup_api.py
@@ -30,21 +30,19 @@ class ClickUpAPI:
             print(f'Failed to connect to Redis: {e}')
             raise HTTPException(status_code=500, detail=f'Failed to connect to Redis: {e}')
 
-    def get_from_cache(self, key: str) -> Union[Dict, None]:
+    def get_from_cache(self, key: str) -> Union[List, None]:
         try:
-            cached_data = self.redis.hgetall(key)  # Utilização de hashes do Redis para melhorar o acesso
+            cached_data = self.redis.get(key)
             if cached_data:
-                return {field.decode('utf-8'): msgpack.unpackb(value) for field, value in cached_data.items()}
+                return msgpack.unpackb(cached_data)
             return None
         except redis.RedisError as e:
             print(f'Redis get error: {e}')
             return None
 
-    def set_in_cache(self, key: str, data: Dict, ttl: int = 600):
+    def set_in_cache(self, key: str, data: List, ttl: int = 600):
         try:
-            for field, value in data.items():
-                self.redis.hset(key, field, msgpack.packb(value))  # Utilização de MessagePack para serialização eficiente
-            self.redis.expire(key, ttl)
+            self.redis.setex(key, ttl, msgpack.packb(data))
         except redis.RedisError as e:
             print(f'Redis set error: {e}')
 
@@ -57,12 +55,17 @@ class ClickUpAPI:
         except httpx.RequestError as e:
             raise HTTPException(status_code=500, detail=f'HTTP error: {str(e)}')
 
-    async def fetch_all_tasks(self, url: str, query: Dict, start_page: int, end_page: int) -> List[Dict]:
+    async def fetch_all_tasks(self, url: str, query: Dict) -> List[Dict]:
         tasks = []
-        for page in range(start_page, end_page):
+        page = 0
+        while True:
             query['page'] = page
             data = await self.fetch_clickup_data(url, query)
-            tasks.extend(data.get('tasks', []))
+            page_tasks = data.get('tasks', [])
+            if not page_tasks:
+                break
+            tasks.extend(page_tasks)
+            page += 1
             await asyncio.sleep(1)  # Add a delay between requests to avoid hitting the rate limit
         return tasks
 
@@ -86,7 +89,7 @@ class ClickUpAPI:
 
     async def get_tasks(self, list_id: str) -> List[Dict[str, Union[str, None]]]:
         url = f'https://api.clickup.com/api/v2/list/{list_id}/task'
-        initial_query = {
+        query = {
             'archived': 'false',
             'include_markdown_description': 'true',
             'page_size': 100,
@@ -97,44 +100,23 @@ class ClickUpAPI:
         end_date_2024 = int(datetime(2024, 12, 31, 23, 59, 59).timestamp() * 1000)  # End of 2024 in milliseconds
 
         if list_id == '192943568':
-            initial_query.update({
+            query.update({
                 'include_closed': 'true',
                 'page_size': 1000,
                 'due_date_gt': start_date_2024,
                 'due_date_lt': end_date_2024,
             })
 
-        tasks = []
-        page = 0
-        while True:
-            cache_key = f'{url}_{json.dumps(initial_query, sort_keys=True)}_page_{page}'
-            cached_data = self.get_from_cache(cache_key)
-            if cached_data:
-                print(f"Page {page}: Data fetched from cache")
-                tasks.extend(cached_data.get('tasks', []))
-            else:
-                print(f"Page {page}: Fetching data from API")
-                data = await self.fetch_all_tasks(url, initial_query, page, page + 1)
-                if not data:
-                    break
-                tasks.extend(data)
-                if page == 0:
-                    self.set_in_cache(cache_key, {f'task_{i}': task for i, task in enumerate(data)}, ttl=600)
-            page += 1
+        cache_key = f'{url}_{json.dumps(query, sort_keys=True)}'
+        cached_tasks = self.get_from_cache(cache_key)
+        if cached_tasks:
+            print("Using cached tasks")
+            return cached_tasks
 
-        # Verificar se os dados do cache estão atualizados
-        cache_key = f'{url}_{json.dumps(initial_query, sort_keys=True)}_all'
-        cached_data = self.get_from_cache(cache_key)
-        if cached_data:
-            cached_tasks = [task for task in cached_data.values()]
-            if len(cached_tasks) == len(tasks):
-                print("Using cached tasks")
-                return cached_tasks
-            else:
-                print("Updating cache with new tasks")
-                self.set_in_cache(cache_key, {f'task_{i}': task for i, task in enumerate(tasks)}, ttl=600)
-        else:
-            self.set_in_cache(cache_key, {f'task_{i}': task for i, task in enumerate(tasks)}, ttl=600)
+        print("Fetching data from API")
+        tasks = await self.fetch_all_tasks(url, query)
+
+        self.set_in_cache(cache_key, tasks, ttl=600)
 
         print(f"Total tasks fetched: {len(tasks)}")
         return tasks
@@ -147,9 +129,15 @@ class ClickUpAPI:
                 'ID': task['id'],
                 'Status': task['status'].get('status', ''),
                 'Name': task.get('name', ''),
-                'Priority': task.get('priority', {}).get('priority', None) if task.get('priority') else None,
-                'Líder': task.get('assignees', [{}])[0].get('username') if task.get('assignees') else None,
-                'Email líder': task.get('assignees', [{}])[0].get('email') if task.get('assignees') else None,
+                'Priority': task.get('priority', {}).get('priority', None)
+                if task.get('priority')
+                else None,
+                'Líder': task.get('assignees', [{}])[0].get('username')
+                if task.get('assignees')
+                else None,
+                'Email líder': task.get('assignees', [{}])[0].get('email')
+                if task.get('assignees')
+                else None,
                 'date_created': self.parse_date(task['date_created']),
                 'date_updated': self.parse_date(task['date_updated']),
             }
