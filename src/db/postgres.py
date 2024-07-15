f87968f7f209d7d5806900a5144facdc814d18ee
venysssssssssss <shablaur7@hotmail.com>
2024-07-15 12:42:11 -0300

    chore: Update PostgresDB save_to_postgres method to include table existence check

--- src/db/postgres.py
+++ src/db/postgres.py
@@ -1,10 +1,15 @@
 import os
-
+from fastapi import logger
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
+import logging
 
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Atualização na classe PostgresDB para incluir verificação da existência da tabela
 class PostgresDB:
     def __init__(
         self,
@@ -22,17 +27,34 @@ class PostgresDB:
         self.engine = create_engine(self.database_url)
 
     def save_to_postgres(self, df: pd.DataFrame, table_name: str):
+        if_exists = 'replace' if self.table_exists(table_name) else 'fail'
         try:
             df.to_sql(
                 table_name,
                 self.engine,
-                if_exists='replace',
+                if_exists=if_exists,
                 index=True,
                 index_label='id',
                 schema=self.schema,
             )
-            print(
+            logger.info(
                 f"Data saved to table '{table_name}' in schema '{self.schema}' of PostgreSQL database"
             )
         except SQLAlchemyError as e:
-            print(f'Error saving data to PostgreSQL: {e}')
+            logger.error(f'Error saving data to PostgreSQL: {e}')
+            raise
+
+    def table_exists(self, table_name: str) -> bool:
+        """
+        Verifica se a tabela já existe no banco de dados.
+        """
+        query = text(f"""
+        SELECT EXISTS (
+            SELECT 1
+            FROM information_schema.tables 
+            WHERE table_schema = '{self.schema}'
+            AND table_name = :table_name
+        );
+        """)
+        with self.engine.connect() as conn:
+            return conn.execute(query, {'table_name': table_name}).scalar()
