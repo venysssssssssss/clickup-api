e2f46f6805a34c43be3f7717ab7199517fb2cd97
venysssssssssss <shablaur7@hotmail.com>
2024-07-15 15:49:12 -0300

    chore: Cache tasks for improved performance in ClickUpAPI get_tasks method

--- src/api/clickup_api.py
+++ src/api/clickup_api.py
@@ -173,9 +173,13 @@ class ClickUpAPI:
             ]
         return result
 
-    async def get_tasks(
-        self, list_id: str
-    ) -> List[Dict[str, Union[str, None]]]:
+    async def get_tasks(self, list_id: str) -> List[Dict[str, Union[str, None]]]:
+        cache_key = f"tasks_{list_id}"
+        cached_tasks = self.cache.get(cache_key)
+        if cached_tasks:
+            print("Using cached data")
+            return cached_tasks
+
         url = f'https://api.clickup.com/api/v2/list/{list_id}/task'
         query = {
             'archived': 'false',
@@ -183,4 +187,5 @@ class ClickUpAPI:
         }
         tasks = await self.fetch_all_tasks(url, query)
         await self.fetch_all_time_in_status(tasks)
+        self.cache.set(cache_key, tasks)
         return tasks
