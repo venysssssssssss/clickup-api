caa68cee6435be6aea17a6a56b75910f73da65b9
venysssssssssss <shablaur7@hotmail.com>
2024-07-17 10:56:50 -0300

    Refactor SQL queries for tasks and related tables

--- dwclickup/models/staging/check_colums.sql
+++
@@ -1,3 +0,0 @@
-select column_name
-from information_schema.columns
-where table_name = 'tasks'
--- dwclickup/models/staging/leaders.sql
+++
@@ -1,8 +0,0 @@
-with leaders as (
-    select
-        distinct leader as leader,
-        leader_email as email
-    from {{ ref('tasks_cleaned') }}
-)
-
-select * from leaders
