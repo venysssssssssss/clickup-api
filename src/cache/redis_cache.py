8331163112fc83afa6bb4eb771764c869d25c03c
venysssssssssss <shablaur7@hotmail.com>
2024-10-22 12:32:58 -0300

    Refactor RedisCache class to handle Redis connection errors and improve error handling

--- src/cache/redis_cache.py
+++ src/cache/redis_cache.py
@@ -1,66 +1,52 @@
 from typing import List, Union
-
-import msgpack
 import redis
+import msgpack
 from fastapi import HTTPException
-
+import ssl
 
 class RedisCache:
-    def __init__(self, redis_url: str):
-        """
-        Inicializa a classe RedisCache.
-
-        Args:
-            redis_url (str): A URL de conexão com o Redis.
-        """
-        self.redis = redis.StrictRedis.from_url(redis_url)
-        self.test_redis_connection()
+    def __init__(self, host: str, port: int, username: str, password: str):
+        """Inicializa a conexão Redis."""
+        try:
+            self.redis = redis.StrictRedis(
+                host=host,
+                port=port,
+                username=username,
+                password=password,
+                ssl=True,
+                ssl_cert_reqs=ssl.CERT_NONE,  # Remova em produção
+                decode_responses=False  # Alterado para False
+            )
+            self.test_redis_connection()
+        except redis.AuthenticationError as e:
+            raise HTTPException(status_code=401, detail=f"Erro de autenticação: {e}")
+        except redis.ConnectionError as e:
+            raise HTTPException(status_code=500, detail=f"Erro de conexão: {e}")
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Erro inesperado: {e}")
 
     def test_redis_connection(self):
-        """
-        Testa a conexão com o Redis.
-
-        Raises:
-            HTTPException: Exceção HTTP caso a conexão com o Redis falhe.
-        """
+        """Verifica se a conexão com o Redis está ativa."""
         try:
             self.redis.ping()
-            print('Conexão com o Redis estabelecida com sucesso')
+            print("Conexão com Redis estabelecida com sucesso.")
         except redis.ConnectionError as e:
-            print(f'Falha ao conectar ao Redis: {e}')
-            raise HTTPException(
-                status_code=500, detail=f'Falha ao conectar ao Redis: {e}'
-            )
+            raise HTTPException(status_code=500, detail=f"Erro ao conectar: {e}")
 
     def get(self, key: str) -> Union[List, None]:
-        """
-        Obtém os dados armazenados no Redis.
-
-        Args:
-            key (str): A chave dos dados a serem obtidos.
-
-        Returns:
-            Union[List, None]: Os dados armazenados no Redis, ou None se não houver dados para a chave especificada.
-        """
+        """Obtém um valor do cache Redis."""
         try:
             cached_data = self.redis.get(key)
             if cached_data:
                 return msgpack.unpackb(cached_data, raw=False)
             return None
         except redis.RedisError as e:
-            print(f'Erro ao obter dados do Redis: {e}')
+            print(f"Erro ao obter dados: {e}")
             return None
 
-    def set(self, key: str, data: List, ttl: int = 600):
-        """
-        Armazena os dados no Redis.
-
-        Args:
-            key (str): A chave dos dados a serem armazenados.
-            data (List): Os dados a serem armazenados.
-            ttl (int, optional): O tempo de vida dos dados em segundos. O padrão é 600 segundos (10 minutos).
-        """
+    def set(self, key: str, value: List, ttl: int = 600):
+        """Define um valor no Redis com um TTL."""
         try:
-            self.redis.setex(key, ttl, msgpack.packb(data, use_bin_type=True))
+            self.redis.setex(key, ttl, msgpack.packb(value, use_bin_type=True))
         except redis.RedisError as e:
-            print(f'Erro ao armazenar dados no Redis: {e}')
+            print(f"Erro ao armazenar dados: {e}")
