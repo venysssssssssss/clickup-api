cc3df8b1d0779afdddfe9499b90391c7fe771558
venysssssssssss <shablaur7@hotmail.com>
2024-10-22 12:32:53 -0300

    Update timeout value in ClickUpAPI fetch_clickup_data method

--- src/api/clickup_api.py
+++ src/api/clickup_api.py
@@ -26,7 +26,7 @@ class ClickUpAPI:
     async def fetch_clickup_data(self, url: str, query: Dict) -> Dict:
         try:
             async with self.semaphore, httpx.AsyncClient(
-                timeout=120.0
+                timeout=300.0
             ) as client:
                 response = await client.get(
                     url, headers=self.headers, params=query
